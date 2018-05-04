ba58fcabb231d07851d2b48c1c5a2bb9816e1cd0
Benjamin Benetti <ben.benetti@shaw.ca>
2018-05-03 19:13:26 -0700

    just add a little struct. commit is really just to sync repos up (got some crazy nesting going on)

--- src/util/misc.h
+++ src/util/misc.h
@@ -1,10 +1,18 @@
 #ifndef MISC_H_
 #define MISC_H_
 #include <stdint.h>
+#include "../config.h"
+#include "../hardware/hardware.h"
 
 /**
   adjust ptr DOWN! in order to align it.
 */
 void * alignPtr(void * ptr, uint16_t align);
+
+
+typedef struct {
+  uint8_t bank;
+  uint32_t pin;
+} Pin;
 
 #endif /*MISC_H_*/
