1af804c4eab53337e8f1109d675b5d6c9eab1d4d
Benjamin Benetti <ben.benetti@shaw.ca>
2018-05-06 23:10:31 -0700

    performance tweaks for PIO to increase display speed

--- src/util/misc.c
+++ src/util/misc.c
@@ -4,9 +4,10 @@ void * alignPtr(void * ptr, uint16_t align){
   return ptr - ((uint32_t)ptr % align);
 }
 
-Pin toPin(uint8_t bank, uint32_t pin){
+Pin toPin(uint8_t bank, uint32_t pin, uint32_t * setReg){
   Pin p;
   p.bank = bank;
   p.pin = pin;
+  p.setReg = setReg;
   return p;
 }
