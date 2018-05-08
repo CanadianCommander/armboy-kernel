2af161645187f4ca48b12a6eee9a00645649e5de
Benjamin Benetti <ben.benetti@shaw.ca>
2018-05-08 14:19:29 -0700

    refactoring + move some functionality out of the kernel and in to hardware lib (it makes more sense).

--- src/util/misc.h
+++ src/util/misc.h
@@ -10,15 +10,4 @@
 void * alignPtr(void * ptr, uint16_t align);
 
 
-typedef struct {
-  uint8_t bank;
-  uint32_t pin;
-  uint32_t * setReg;
-} Pin;
-
-Pin toPin(uint8_t bank, uint32_t pin, uint32_t * setReg);
-
-#define SET_PIN(p,v) setPin(p.bank,p.pin,v)
-#define READ_PIN(p) readPin(p.bank,p.pin)
-
 #endif /*MISC_H_*/
